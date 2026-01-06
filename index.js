c19e05132991e03ecc9d2e81d629af173e8b8ad9
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2026-01-06 16:01:37 +0500

    added the coding fact

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ console.log("Hello! I'm your coding fun fact guide!");
 let botName = "Ahtisham";
 let botLocation = "Lahore";
 let favoriteLanguage = "JavaScript";
-console.log(`My name is ${botName} and I live on ${favoriteLanguage}.`)
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+let codingFact = `${favoriteLanguage} is tricky language`;
+console.log(codingFact);
